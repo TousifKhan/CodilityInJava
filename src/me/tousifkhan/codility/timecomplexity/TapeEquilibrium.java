243c5642f595cebcc8c04774c96de59eae27fa5c
Tousif Ahmed <tousifkhan510@gmail.com>
2023-08-27 12:55:26 +0530

    timecomplexity > TapeEquilibrium - another solution

--- src/me/tousifkhan/codility/timecomplexity/TapeEquilibrium.java
+++ src/me/tousifkhan/codility/timecomplexity/TapeEquilibrium.java
@@ -6,7 +6,7 @@ import java.util.stream.IntStream;
 //https://app.codility.com/programmers/lessons/3-time_complexity/tape_equilibrium/
 public class TapeEquilibrium {
 
-    public int solutions(int[] A) {
+    public int solution(int[] A) {
         int minDiff = Integer.MIN_VALUE;
         int leftSum = 0;
         int rightSum = 0;
@@ -26,8 +26,31 @@ public class TapeEquilibrium {
         return minDiff;
     }
 
+    /***
+     * Another approach
+     * @param A as array
+     * @return int
+     */
+    public int solution2(int[] A) {
+        int leftSum = A[0];
+        int rightSum = 0;
+        for (int n: A) {
+            rightSum +=n;
+        }
+        rightSum -= leftSum;
+
+        int diff = Math.abs(leftSum - rightSum);
+        for (int i = 1; i < A.length; i++) {
+            leftSum+=A[i];
+            rightSum-=A[i];
+            int currentDiff = Math.abs(leftSum - rightSum);
+            if (diff > currentDiff) diff = currentDiff;
+        }
+        return diff;
+    }
+
     public static void main(String[] args) {
         TapeEquilibrium tapeEquilibrium = new TapeEquilibrium();
-        System.out.println(tapeEquilibrium.solutions(new int[] {3,1,2,4,3}));
+        System.out.println(tapeEquilibrium.solution2(new int[] {3,1,2,4,3}));
     }
 }
